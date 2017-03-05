d5947ffe9fe96f920d8fd9537b0a41f9b238d616
Luv Gangwani <luv.gangwani@gmail.com>
2017-03-05 12:42:22 +0530

    Healthcarehomoeo - Add content for about me and contact us

--- contact_me.php
+++ contact_me.php
@@ -1,3 +1,76 @@
 <div class="container-fluid">
-	Content for Contact Me
+
+	<p>Please feel free to contact me and make an appointment.</p>
+	<div class="panel panel-default">
+		<table class="table table-bordered">
+			<tr>
+				<td rowspan="2" style="vertical-align: middle">Practicing at</td>
+				<td>
+					<b>
+						<u>
+							Jeevandeep Clinic
+						</u>
+					</b>
+					<p>Radhakunj Building Basement,</p>
+					<p>Ram Maruti Road, Thane(W) - 400 602</p>
+					<p>
+						<u>Timings: (Monday - Saturday)</u>
+						<p>7:00pm to 9:00pm</p>
+					</p>
+					<p>
+						<u>Contact Number</u>
+						<p>022-25360005 | 25446506</p>
+					</p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<b>
+						<u>
+							Aditya Polyclinic
+						</u>
+					</b>
+					<p>A-7.6-3, Happy Valley,</p>
+					<p>Behind ICICI ATM,</p>
+					<p>Near Tikujiniwadi, Ghodbunder Road,</p>
+					<p>Chitalsar Manpada, Thane(W)</p>
+					<p>
+						<u>Timings:</u>
+						<p>Morning - 10:00am to 1:00pm</p>
+						<p>Evening(By prior appointments) - </p>
+						<p>5:30pm to 6:30pm</p>
+						<p>9:30pm to 10:30pm</p>
+					</p>
+				</td>
+			</tr>
+			<tr>
+				<td>Email ID</td>
+				<td><a href="mailto:drgangwani@yahoo.com">drgangwani@yahoo.com</a></td>
+			</tr>
+			<tr>
+				<td>Contact Number</td>
+				<td>+91-9323596633 | 9619313790</td>
+			</tr>
+		</table>
+	</div>
+	<hr>
+	<div class="h3"><u>Contact me via email</u></div>
+	<div class="email-form">
+		<form class="form-horizontal" id="form_send_email">
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="sender_email_id">Email ID:</label>
+				<div class="col-sm-10">
+					<input class="form-control" name="sender_email_id" id="sender_email_id" placeholder="Enter your Email ID" type="text">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="mail_content">Content:</label>
+				<div class="col-sm-10">
+					<textarea class="form-control" placeholder="Add you content here" name="mail_content" id="mail_content" rows="5"></textarea>
+				</div>
+			</div>
+			<button type="submit" class="btn btn-default" id="btn_send_email">Send</button>
+			<button type="reset" class="btn btn-default">Reset</button>
+		</form>
+	</div>
 </div>
