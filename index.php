993cef0886f4caa9f5e6603ab35cdc02196a38af
Luv Gangwani <luv.gangwani@gmail.com>
2017-03-21 23:23:25 +0530

    HealthcareHomoeo - Mobile view

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 	<head>
 		<title>HealthCare Homoeopathy</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="js/node_modules/jquery-ui-dist/jquery-ui.min.css">
 		<link rel="stylesheet" type="text/css" href="js/node_modules/bootstrap/dist/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="js/node_modules/bootstrap/dist/css/bootstrap-theme.min.css">
