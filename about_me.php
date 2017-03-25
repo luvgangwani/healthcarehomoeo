2a360cdc14aa353468285c3753f564d1a57cabdc
Luv Gangwani <luv.gangwani@gmail.com>
2017-03-25 16:01:17 +0530

    HealthcareHomoeo - Add hyperlink reference

--- about_me.php
+++ about_me.php
@@ -2,7 +2,7 @@
 	<img class="img-rounded img-thumbnail" src="images/image.jpg" width="200px" height="200px">
 	<div class="about-me-text">
 		<p>Greetings to all viewers!!</p>
-		<p>Homoepathy, as stated by its creator Samuel Hahnemann, is based on the ideology of <i> like cures like </i>. This means that substance that causes symptoms of a disease in a healthy individual is likely to become the cure for similar symptoms in a sick individual.Having a professional practice of more than 30 years in Homoeopathy based on this ideology I wish to offer my experience in Homoeopathic treatment to you all.</p>
-		<p>Please visit the <i>Contact Me</i> section to get make an appointment</p>
+		<p>Homoepathy, as stated by its creator <a href="https://en.wikipedia.org/wiki/Samuel_Hahnemann">Samuel Hahnemann</a>, is based on the ideology of <i> like cures like </i>. This means that substance that causes symptoms of a disease in a healthy individual is likely to become the cure for similar symptoms in a sick individual.Having a professional practice of more than 30 years in Homoeopathy based on this ideology I wish to offer my experience in Homoeopathic treatment to you all.</p>
+		<p>Please visit the <a href="javascript:void(0)" id="lnk_about_me_contact_me">Contact Me</a> section to get make an appointment</p>
 	</div>
 </div>
